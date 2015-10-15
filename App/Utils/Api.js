07ed0d1fcce30b592b22031d7f4fe685aaa910c4
Pavel Razuvayev <prx124@my.utsa.edu>
2015-10-15 18:15:34 -0500

    add api.js template

---
+++ App/Utils/Api.js
@@ -0,0 +1,28 @@
+var Api = {
+  getLocation(city) {
+    // Location
+  },
+  getSummary() {
+    // Summary
+  },
+  getTemperature() {
+    // Temperature
+  },
+  getHumidity() {
+    // Humidity
+  },
+  getTimzone() {
+    // Time
+  },
+  getIcon() {
+    // Icon
+  },
+  getPrecipChance() {
+    // Precipitation probability
+  },
+  getCurrentTime() {
+    // Current time
+  }
+};
+
+module.exports = Api;
